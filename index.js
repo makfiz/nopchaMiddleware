cdc65833930686224fdd40ed8e92d6814581ecb1
makfiz <99556605+makfiz@users.noreply.github.com>
2024-12-13 12:40:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.all('*', async (req, res) => {
     try {
         // Формируем параметры для запроса
-        const targetUrl = `https://api.nopcha.com${req.originalUrl}`; // Перенаправление на тот же путь
+        const targetUrl = `https://api.nopcha.com${"/status?key=I-PG88N6RRYKUY"}`; // Перенаправление на тот же путь
         // console.log(req.originalUrl)
         const options = {
             method: req.method,
